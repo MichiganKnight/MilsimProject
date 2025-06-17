64f5a73fcff104275644dadc63c1b73d53a26cf8
Andrew Fleming <michiganknight2@gmail.com>
2025-06-17 01:17:19 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,73 @@
 #include <SDL.h>
 #include <iostream>
 
-int main(int argc, char* args[]) {
+// Vertex shader source code
+const char *vertexShaderSource = R"glsl(
+#version 330 core
+layout(location = 0) in vec3 aPos;
+
+void main() {
+    gl_Position = vec4(aPos, 1.0);
+}
+)glsl";
+
+// Fragment shader source code
+const char *fragmentShaderSource = R"glsl(
+#version 330 core
+out vec4 FragColor;
+
+void main() {
+    FragColor = vec4(0.2, 0.7, 0.3, 1.0); // green color
+}
+)glsl";
+
+GLuint CompileShader(GLenum type, const char *source) {
+    GLuint shader = glCreateShader(type);
+
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+
+    GLint success;
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        std::cout << "Shader Compilation Error: " << infoLog << std::endl;
+    }
+
+    return shader;
+}
+
+GLuint CreateShaderProgram(const char *vertexSrc, const char *fragmentSrc) {
+    GLuint vertexShader = CompileShader(GL_VERTEX_SHADER, vertexSrc);
+    GLuint fragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentSrc);
+
+    GLuint program = glCreateProgram();
+
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+
+    glLinkProgram(program);
+
+    GLint success;
+
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(program, 512, nullptr, infoLog);
+        std::cout << "Shader Linking Error: " << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
+
+int main(int argc, char *args[]) {
     SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "1");
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -15,7 +81,8 @@ int main(int argc, char* args[]) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
     // Create a Window Centered on the Screen
-    SDL_Window* window = SDL_CreateWindow("Milsim FPS", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    SDL_Window *window = SDL_CreateWindow("Milsim FPS", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600,
+                                          SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
     SDL_GLContext glContext = SDL_GL_CreateContext(window);
 
@@ -26,13 +93,37 @@ int main(int argc, char* args[]) {
         return 1;
     }
 
-    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+    if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
         std::cerr << "Failed to Initialize GLAD. SDL_Error: " << SDL_GetError() << std::endl;
         SDL_GL_DeleteContext(glContext);
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
     }
+
+    GLuint shaderProgram = CreateShaderProgram(vertexShaderSource, fragmentShaderSource);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // Left
+        0.5f, -0.5f, 0.0f, // Right
+        0.0f, 0.5f, 0.0f // Top
+    };
+
+    GLuint VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     SDL_GL_SetSwapInterval(1); // Enable VSync
 
@@ -60,6 +151,11 @@ int main(int argc, char* args[]) {
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
         // Swap Buffers
         SDL_GL_SwapWindow(window);
 
@@ -68,6 +164,11 @@ int main(int argc, char* args[]) {
     }
 
     // Clean Up and Close Window
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
